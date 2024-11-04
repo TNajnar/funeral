c1988f3cde2aca190db0fc3faebdc3c82414e7ef
Tomas Kopulety <tomas.kopulety@regiojet.cz>
2024-11-04 06:22:38 +0100

    Create new java project

---
+++ funeral_backend/src/main/java/cz/tomaskopulety/funeral_backend/FuneralBackendApplication.java
@@ -0,0 +1,13 @@
+package cz.tomaskopulety.funeral_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FuneralBackendApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(FuneralBackendApplication.class, args);
+  }
+
+}
