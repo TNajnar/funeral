e35bef4a8c1222e1c7410a1f339e080fed9db3c3
Tomas Najnar <tomas.najnar@seznam.cz>
2024-11-24 16:05:52 +0100

    Some dashboard changes

--- src/app/pages/authorized/dashboard/dashboard.component.ts
+++ src/app/pages/authorized/dashboard/dashboard.component.ts
@@ -1,12 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, inject, LOCALE_ID } from '@angular/core';
+import { DatePipe, registerLocaleData } from '@angular/common';
+import { MatButtonModule } from '@angular/material/button';
+import localeCs from '@angular/common/locales/cs';
+
+import { GraphComponent } from '@app/ui/graph/graph.component';
+
+registerLocaleData(localeCs);
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [],
+  imports: [MatButtonModule, GraphComponent],
   templateUrl: './dashboard.component.html',
-  styleUrl: './dashboard.component.css'
+  host: {
+    class: 'flex flex-col gap-28 w-full'
+  },
+  providers: [
+    DatePipe,
+    { provide: LOCALE_ID, useValue: 'cs' }
+  ]
 })
 export class DashboardComponent {
+  protected _currentMonth: string | null;
 
+  private _datePipe: DatePipe = inject(DatePipe);
+
+  constructor() {
+    this._currentMonth = this._datePipe.transform(new Date(), 'MMMM');
+  }
 }
