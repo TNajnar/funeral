d438ba78f00ee69d7c18003a8ba3e587f1078ca5
Tomas Najnar <tomas.najnar@seznam.cz>
2025-01-06 12:54:11 +0100

    Improve Amount Modal

--- src/app/pages/authorized/warehouse-control/warehouse-table/product-amount-change-menu/change-amount-tab/change-amount-tab.component.ts
+++ src/app/pages/authorized/warehouse-control/warehouse-table/product-amount-change-menu/change-amount-tab/change-amount-tab.component.ts
@@ -1,52 +1,48 @@
-import { Component, inject, Input, output } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatTooltipModule } from '@angular/material/tooltip';
 
-import { WarehouseService } from '@pages/authorized/warehouse-control/services/warehouse.service';
-import {
-  WarehouseTableGatewayService,
-} from '@pages/authorized/warehouse-control/gateways/warehouse-table.gateway.service';
 import type { TWarehouseItem } from '@app/pages/authorized/warehouse-control/utils/warehouse-control.gateway.model';
+import { AmountTabBaseComponent } from '../amount-tab-base/amount-tab-base.component';
+import { AmountTabBaseTemplateComponent } from '../amount-tab-base/amount-tab-base-template.component';
 import { warehouseControl } from '@lib/staticTexts';
 
 @Component({
   selector: 'app-change-amount-tab',
   standalone: true,
-  imports: [FormsModule, MatButtonModule, MatIconModule, MatTooltipModule],
+  imports: [FormsModule, MatButtonModule, MatIconModule, MatTooltipModule, AmountTabBaseTemplateComponent],
   templateUrl: './change-amount-tab.component.html',
   styleUrl: './change-amount-tab.component.css',
-  host: { class: 'flex flex-col gap-4 pt-3 px-3 pb-1 mt-2 w-full text-sm' },
 })
-export class ChangeAmountTabComponent {
-  protected _texts = warehouseControl.table.amountMenu.change;
-  productAmount: number | undefined;
-
+export class ChangeAmountTabComponent extends AmountTabBaseComponent {
   @Input({ required: true }) product!: TWarehouseItem;
-  onClose = output<void>();
+  @Output() onClose = new EventEmitter<void>();
 
-  private _warehouseService: WarehouseService = inject(WarehouseService);
-  private _gateway: WarehouseTableGatewayService = inject(WarehouseTableGatewayService);
+  constructor() {
+    super();
+    this._texts = warehouseControl.table.amountMenu.change;
+  }
 
-  onSubmit(): void {
-    if (!this.productAmount) {
+  protected override _onSubmit(): void {
+    if (!this._productAmount) {
       return;
     }
 
-    this._gateway.changeProductAmount(this.product.productId, this.productAmount).subscribe({
+    this._gateway.changeProductAmount(this.product.productId, this._productAmount).subscribe({
       next: (responseProduct: TWarehouseItem) => {
         this.product.inStock = responseProduct.inStock;
         this._warehouseService.updateWarehouseCache();
       },
       complete: () => {
-        this.handleClose();
+        this._handleClose();
       },
     });
   }
 
-  handleClose(): void {
-    this.productAmount = undefined;
+  protected override _handleClose(): void {
+    this._productAmount = undefined;
     this.onClose.emit();
   }
 }
--- src/app/pages/authorized/warehouse-control/warehouse-table/product-amount-change-menu/stock-in-amount-tab/stock-in-amount-tab.component.ts
+++ src/app/pages/authorized/warehouse-control/warehouse-table/product-amount-change-menu/stock-in-amount-tab/stock-in-amount-tab.component.ts
@@ -1,42 +1,36 @@
-import { Component, inject, Input, output } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatTooltipModule } from '@angular/material/tooltip';
 
-import { WarehouseService } from '@pages/authorized/warehouse-control/services/warehouse.service';
-import {
-  WarehouseTableGatewayService,
-} from '@pages/authorized/warehouse-control/gateways/warehouse-table.gateway.service';
-import { ErrorService } from 'services/error.service';
 import type { TWarehouseItem } from '@app/pages/authorized/warehouse-control/utils/warehouse-control.gateway.model';
+import { AmountTabBaseComponent } from '../amount-tab-base/amount-tab-base.component';
+import { AmountTabBaseTemplateComponent } from '../amount-tab-base/amount-tab-base-template.component';
 import { warehouseControl } from '@lib/staticTexts';
 
 @Component({
   selector: 'app-stock-in-amount-tab',
   standalone: true,
-  imports: [FormsModule, MatButtonModule, MatIconModule, MatTooltipModule],
+  imports: [FormsModule, AmountTabBaseTemplateComponent, MatButtonModule, MatIconModule, MatTooltipModule],
   templateUrl: './stock-in-amount-tab.component.html',
   styleUrl: './stock-in-amount-tab.component.css',
-  host: { class: 'flex flex-col gap-4 pt-3 px-3 pb-1 mt-2 w-full' },
 })
-export class StockInAmountTabComponent {
-  protected _texts = warehouseControl.table.amountMenu.stockIn;
-  productAmount: number | undefined;
-
+export class StockInAmountTabComponent extends AmountTabBaseComponent {
   @Input({ required: true }) product!: TWarehouseItem;
-  onClose = output<void>();
+  @Output() onClose = new EventEmitter<void>();
 
-  private _warehouseService: WarehouseService = inject(WarehouseService);
-  private _gateway: WarehouseTableGatewayService = inject(WarehouseTableGatewayService);
-  private _errorService: ErrorService = inject(ErrorService);
+  constructor() {
+    super();
+    this._texts = warehouseControl.table.amountMenu.stockIn;
+  }
 
-  onSubmit(): void {
-    if (!this.productAmount) {
+  protected override _onSubmit(): void {
+    if (!this._productAmount) {
       return;
     }
 
-    const parsedValue = Number(this.productAmount);
+    const parsedValue = Number(this._productAmount);
 
     if (isNaN(parsedValue)) {
       this._errorService.showError(this._texts.error);
@@ -49,13 +43,13 @@ export class StockInAmountTabComponent {
         this._warehouseService.updateWarehouseCache();
       },
       complete: () => {
-        this.handleClose();
+        this._handleClose();
       },
     });
   }
 
-  handleClose(): void {
-    this.productAmount = undefined;
+  protected override _handleClose(): void {
+    this._productAmount = undefined;
     this.onClose.emit();
   }
 }
