884cc098a66a34612fb21d7fb818a9f2232d3e24
Tomas Najnar <tomas.najnar@seznam.cz>
2025-01-15 16:36:50 +0100

    Better persisting

--- src/app/pages/authorized/warehouse-control/services/warehouse.service.ts
+++ src/app/pages/authorized/warehouse-control/services/warehouse.service.ts
@@ -1,12 +1,13 @@
 import { inject, Injectable, signal } from '@angular/core';
 import { BehaviorSubject, Observable } from 'rxjs';
 import { MatTableDataSource } from '@angular/material/table';
+import { MatPaginator } from '@angular/material/paginator';
 
 import { WarehouseTableFiltersService } from './warehouse-table-filters.service';
 import { WarehouseCacheService } from './warehouse-cache.service';
 import type { TFilterOptions } from '../utils/warehouse-control.model';
 import type { TCategory, TWarehouseItem } from '../utils/warehouse-control.gateway.model';
-import { STATIC_CATEGORY_ITEM } from '../utils/consts';
+import { DEFAULT_PAGINATION_SIZE, STATIC_CATEGORY_ITEM } from '../utils/consts';
 
 @Injectable({
   providedIn: 'root'
@@ -18,6 +19,7 @@ export class WarehouseService {
   private _tableDataSource = new MatTableDataSource<TWarehouseItem>();
   private _categories = signal<TCategory[]>([]);
   private _activeTab = signal<number>(STATIC_CATEGORY_ITEM.id);
+  private _selectedPagination = signal<number>(DEFAULT_PAGINATION_SIZE);
   isLoading = signal<boolean>(false);
 
   private _warehouseTableFilters = inject(WarehouseTableFiltersService);
@@ -38,10 +40,6 @@ export class WarehouseService {
     return this._onWarehouseTableChange$.asObservable();
   }
 
-  get tableDataSource(): MatTableDataSource<TWarehouseItem> {
-    return this._tableDataSource;
-  }
-
   get filterOptions(): TFilterOptions {
     return this._warehouseTableFilters.filterOptions;
   }
@@ -50,30 +48,32 @@ export class WarehouseService {
     return this._onCategoryChange$.asObservable();
   }
 
+  get tableDataSource(): Readonly<MatTableDataSource<TWarehouseItem>> {
+    return this._tableDataSource;
+  }
+
+  get selectedPagination(): Readonly<number> {
+    return this._selectedPagination();
+  }
+
+  setWarehouseData(newCategories: TCategory[], warehouseItems: TWarehouseItem[], pageSize?: number): void {
+    this._categories.set(newCategories);
+    this._selectedPagination.set(pageSize || DEFAULT_PAGINATION_SIZE);
+    this._updateWarehouseItems(warehouseItems);
+  }
+
   updateWarehouseData(): void {
     this._notifyWarehouseItemsChange$();
     this._updateCache();
   }
 
   addWarehouseItem(item: TWarehouseItem): void {
-    this.updateWarehouseItems([item, ...this._warehouseItems$.getValue()]);
+    this._updateWarehouseItems([item, ...this._warehouseItems$.getValue()]);
   }
 
   deleteWarehouseItem(productId: number): void {
     const updatedItems = this._warehouseItems$.getValue().filter(item => item.productId !== productId);
-    this.updateWarehouseItems(updatedItems);
-  }
-
-  updateWarehouseItems(items: TWarehouseItem[]): void {
-    this._warehouseItems$.next(items);
-    this._tableDataSource.data = items;
-    this._notifyWarehouseItemsChange$();
-    this._updateCache();
-  }
-
-  setCategories(newCategories: TCategory[]): void {
-    this._categories.set(newCategories);
-    this._updateCache();
+    this._updateWarehouseItems(updatedItems);
   }
 
   createNewCategory(newCategory: TCategory): void {
@@ -98,6 +98,15 @@ export class WarehouseService {
     this._tableDataSource.filter = JSON.stringify(this.filterOptions);
   }
 
+  setTableDataPaginator(paginator: MatPaginator): void {
+    this._tableDataSource.paginator = paginator;
+  }
+
+  setPaginationSize(pageSize: number): void {
+    this._selectedPagination.set(pageSize);
+    this._updateCache();
+  }
+
   private _initializeTable(): void {
     this._tableDataSource.filterPredicate = this._warehouseTableFilters.createFilterPredicate();
   }
@@ -106,9 +115,17 @@ export class WarehouseService {
     this._onWarehouseTableChange$.next(true);
   }
 
+  private _updateWarehouseItems(warehouseItems: TWarehouseItem[]): void {
+    this._warehouseItems$.next(warehouseItems);
+    this._tableDataSource.data = warehouseItems;
+    this._notifyWarehouseItemsChange$();
+    this._updateCache();
+  }
+
   private _updateCache(): void {
     this._cacheService.saveToStorage({
       categories: this._categories(),
+      tablePagination: this._selectedPagination(),
       warehouseItems: this._warehouseItems$.getValue(),
     });
   }
